af9582e0e1b868c6aa7acadee06cc4aa57b7a01f
Ting Wang <tting.wang@gmail.com>
2012-06-29 08:32:30 +0200

    Adjust the db info

--- application/config/database.php
+++ application/config/database.php
@@ -69,10 +69,10 @@ return array(
 
         'mysql' => array(
                 'driver'   => 'mysql',
-                'host'     => 'localhost',
-                'database' => 'notte',
-                'username' => 'root',
-                'password' => 'root',
+                'host'     => getenv('MYSQL_DB_HOST'),
+                'database' => getenv('MYSQL_DB_NAME'),
+                'username' => getenv('MYSQL_USERNAME'),
+                'password' => getenv('MYSQL_PASSWORD'),
                 'charset'  => 'utf8',
                 'prefix'   => '',
         ),
